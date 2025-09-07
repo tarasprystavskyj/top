6d99f1aab915406985df03eee2a5e9454cd94a7b
Taras Prystavskyj <tarasprystavskyj@gmail.com>
2025-09-07 05:14:09 +0300

    chore: configure API port and add swr

--- UI/frontend/next.config.js
+++ UI/frontend/next.config.js
@@ -1,1 +1,14 @@
-module.exports = { reactStrictMode: true, output: 'standalone' };
+const API_URL = process.env.BACKEND_URL || 'http://vps2.happyuser.info:8001';
+
+module.exports = {
+  reactStrictMode: true,
+  output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${API_URL}/api/:path*`,
+      },
+    ];
+  },
+};
