c1a02cef0edaa499b07f827e63a997ed96703c86
Taras Prystavskyj <tarasprystavskyj@gmail.com>
2025-09-07 05:38:24 +0300

    Proxy API requests to backend

--- UI/frontend/utils/api.ts
+++ UI/frontend/utils/api.ts
@@ -1,4 +1,3 @@
-const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://vps2.happyuser.info:8001';
 export function apiFetch(path: string, options?: RequestInit) {
-  return fetch(`${API_URL}${path}`, options);
+  return fetch(path, options);
 }
