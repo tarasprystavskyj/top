ab45ad66b1a9fd48c196bd735355f0a98d8d6999
Taras Prystavskyj <tarasprystavskyj@gmail.com>
2025-09-09 09:06:48 +0300

    feat: add debug logs and disable app dir

--- UI/frontend/next.config.js
+++ UI/frontend/next.config.js
@@ -3,6 +3,7 @@ const API_URL = process.env.BACKEND_URL || 'http://127.0.0.1:8001';
 module.exports = {
   reactStrictMode: true,
   output: 'standalone',
+  experimental: { appDir: false },
   async rewrites() {
     return [
       {
